2b535e7bb143bcb90c9c682baa2ade1e0ff58b23
استغفر الله < @DESKTOP-ST7KD6R>
2017-03-08 19:52:43 +0200

    Add project files.

---
+++ firstprogram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int abdo=100+1000 ;
+            Console.WriteLine("abdo= "+ (abdo-10));
+            Console.ReadKey();         
+        }
+    } 
+}
